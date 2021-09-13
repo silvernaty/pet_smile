685f716f0f7a15765a180d941a0d7ab21704e690
Natalia Iturriaga <silvernaty@gmail.com>
2021-09-13 01:59:04 -0300

    first documents

---
+++ src/java/bean/petSmileBean.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author darkn
+ */
+@Named(value = "petSmileBean")
+@Dependent
+public class petSmileBean {
+
+    private int rut;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String correo;
+    private String telefono;
+    private String nombreMascota;
+    private String tipoMascota;
+    private String edad;
+    private String nombreUsuario;
+    private String password;
+    
+    
+    
+    public petSmileBean() {
+    }
+
+    public petSmileBean(int rut, String nombre, String apellido, String direccion, String correo, String telefono, String nombreMascota, String tipoMascota, String edad, String nombreUsuario, String password) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.nombreMascota = nombreMascota;
+        this.tipoMascota = tipoMascota;
+        this.edad = edad;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombreMascota() {
+        return nombreMascota;
+    }
+
+    public void setNombreMascota(String nombreMascota) {
+        this.nombreMascota = nombreMascota;
+    }
+
+    public String getTipoMascota() {
+        return tipoMascota;
+    }
+
+    public void setTipoMascota(String tipoMascota) {
+        this.tipoMascota = tipoMascota;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
